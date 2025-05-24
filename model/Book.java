55728b009f870080fcc7b33c12f4e64e7d2bbb3e
Raunakkumar007 <raunak.24scse1010078@galgotiasuniversity.ac.in>
2025-05-24 20:17:17 +0530

    Add files via upload

---
+++ model/Book.java
@@ -0,0 +1,46 @@
+package model;
+import java.util.Objects;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+
+    public Book(int id, String name, String author) {
+        this.id = id;
+        this.name = name.trim();
+        this.author = author.trim();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return id + ": " + name + " by " + author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    Book b1 = new Book(1, "Rich Dad Poor Dad", "Robert Kiyosaki");
+
+}
